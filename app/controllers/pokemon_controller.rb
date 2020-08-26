246d4ee222d8dc221fe16bfb567e4f5d12f70367
mrchvz <chavezmmarie@gmail.com>
2020-08-26 16:13:17 -0700

    post revision Ivan

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,9 +1,11 @@
 class PokemonController < ApplicationController
   def index
     @pokemons = Pokemon.all
+    render(json: @pokemons)
   end
 
   def show
-    @pokemon = Pokemon.find_by(name: params.fetch(:id))
+    @pokemon = Pokemon.find_by(name: params.fetch(:name))
+    render(json: @pokemon)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   root 'pokemon#index'
-  get '/index' => 'pages#index'
-  get '/show:id' => 'pages#show'
-  
-  resources :pokemon
-  get '/pokemon/:id', to: 'pokemon#show'
-  get '/pokemon', to: 'pokemon#index'
+  resources(:pokemon, { only: [:show, :index], param: :name })
 end
